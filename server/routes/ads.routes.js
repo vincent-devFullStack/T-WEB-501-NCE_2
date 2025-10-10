876c32486f5eb16248bdbaa723dc6490b5e32bad
vince.dev <vincent.silvestri@epitech.eu>
2025-10-10 16:58:55 +0200

    suppression dossier obsolete

--- server/routes/ads.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { listAds } from "../controller/ads.controller.js";
-const r = Router();
-
-r.get("/", listAds);
-
-export default r;
--- server/routes/companies.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { listCompanies } from "../controller/companies.controller.js";
-const r = Router();
-
-r.get("/", listCompanies);
-
-export default r;
